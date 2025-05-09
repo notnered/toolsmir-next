11ab7d661359839e00562feb9eddc28e6bc8a351
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-10 03:35:10 +0700

    created default container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
+            <body className={`${geistMono.variable} container mx-auto antialiased`}>
                 {children}
             </body>
         </html>
