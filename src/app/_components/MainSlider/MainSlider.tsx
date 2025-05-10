63937c142abd9f05df97d46349069e1dfdc47f4c
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-10 16:35:45 +0700

    updated main slider with breadcrumbs icons

--- src/app/_components/MainSlider/MainSlider.tsx
+++ src/app/_components/MainSlider/MainSlider.tsx
@@ -3,33 +3,45 @@ import slidePic from '../../../../public/slide.png';
 
 export default function MainSlider() {
     return (
-        <div className='flex justify-between gap-x-4 py-4'>
-            <div className='w-full'>
-                <Image
-                    className='h-80 object-cover'
-                    src={slidePic.src}
-                    alt='Slide'
-                    width={slidePic.width}
-                    height={slidePic.height}
-                />
+        // slider
+        <div>
+            <div className='flex justify-between gap-x-4 py-4'>
+                <div className='w-full'>
+                    <Image
+                        className='h-80 object-cover'
+                        title='slide'
+                        src={slidePic.src}
+                        alt='Slide'
+                        width={slidePic.width}
+                        height={slidePic.height}
+                    />
+                </div>
+                <div className='w-full'>
+                    <Image
+                        className='h-80 object-cover'
+                        title='slide'
+                        src={slidePic.src}
+                        alt='Slide'
+                        width={slidePic.width}
+                        height={slidePic.height}
+                    />
+                </div>
+                <div className='w-full'>
+                    <Image
+                        className='h-80 object-cover'
+                        title='slide'
+                        src={slidePic.src}
+                        alt='Slide'
+                        width={slidePic.width}
+                        height={slidePic.height}
+                    />
+                </div>
             </div>
-            <div className='w-full'>
-                <Image
-                    className='h-80 object-cover'
-                    src={slidePic.src}
-                    alt='Slide'
-                    width={slidePic.width}
-                    height={slidePic.height}
-                />
-            </div>
-            <div className='w-full'>
-                <Image
-                    className='h-80 object-cover'
-                    src={slidePic.src}
-                    alt='Slide'
-                    width={slidePic.width}
-                    height={slidePic.height}
-                />
+            {/* icons */}
+            <div className='flex justify-center gap-x-1'>
+                <div className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors'></div>
+                <div className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors'></div>
+                <div className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors'></div>
             </div>
         </div>
     );
