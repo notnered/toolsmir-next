a02d98d1641462ed2f8fafeea72fefa4dade98ef
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-12 15:07:39 +0700

    optimized mainslider block

--- src/app/_components/MainSlider/MainSlider.tsx
+++ src/app/_components/MainSlider/MainSlider.tsx
@@ -1,47 +1,39 @@
 import Image from 'next/image';
 import slidePic from '../../../../public/slide.png';
 
+const slides = [
+    {id: 0, picture: slidePic, name: 'Слайдер'},
+    {id: 1, picture: slidePic, name: 'Слайдер'},
+    {id: 2, picture: slidePic, name: 'Слайдер'},
+    // {id: 3, picture: slidePic, name: 'Слайдер'},
+]
+
 export default function MainSlider() {
     return (
-        // slider
         <div>
-            <div className='flex justify-between gap-x-4 py-4'>
-                <div className='w-full'>
-                    <Image
-                        className='h-80 object-cover'
-                        title='slide'
-                        src={slidePic.src}
-                        alt='Slide'
-                        width={slidePic.width}
-                        height={slidePic.height}
-                    />
-                </div>
-                <div className='w-full'>
-                    <Image
-                        className='h-80 object-cover'
-                        title='slide'
-                        src={slidePic.src}
-                        alt='Slide'
-                        width={slidePic.width}
-                        height={slidePic.height}
-                    />
-                </div>
-                <div className='w-full'>
-                    <Image
-                        className='h-80 object-cover'
-                        title='slide'
-                        src={slidePic.src}
-                        alt='Slide'
-                        width={slidePic.width}
-                        height={slidePic.height}
-                    />
-                </div>
+            <div className='flex overflow-x-hidden gap-x-4 py-4'>
+                {slides.slice(0, 5).map((slide, index) => {
+                    return (
+                        <div key={slide.id} className='w-full min-w-1/3'>
+                            <Image
+                                className='h-80 object-cover rounded-md'
+                                title={slide.name}
+                                src={slide.picture.src}
+                                alt={slide.name}
+                                width={slide.picture.width}
+                                height={slide.picture.height}
+                            />
+                        </div>
+                    )
+                })}
             </div>
             {/* icons */}
             <div className='flex justify-center gap-x-1'>
-                <div className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors'></div>
-                <div className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors'></div>
-                <div className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors'></div>
+                {Array(slides.slice(0, 5).length).fill('').map((element, index) => {
+                    return (
+                        <div key={index} className='size-2.5 rounded-full bg-gray-900/85 hover:bg-gray-900 transition-colors cursor-pointer'></div>
+                    )
+                })}
             </div>
         </div>
     );
