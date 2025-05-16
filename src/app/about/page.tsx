cca33805e1298a0505460a9f9ba3c0a7ca0b8cb8
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-16 18:19:46 +0700

    created about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "../_components/Footer/Footer";
+import Nav from "../_components/Nav/Nav";
+
+export default function About(){
+    return (
+        <>
+        <Nav />
+        <main>
+            About
+        </main>
+        <Footer />
+        </>
+    )
+}
