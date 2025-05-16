fbdae4d7fc1958c51f5c512a69e9b396b76e1824
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-16 18:20:00 +0700

    created contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "../_components/Footer/Footer";
+import Nav from "../_components/Nav/Nav";
+
+export default function Contact(){
+    return (
+        <>
+        <Nav />
+        <main>
+            Contact
+        </main>
+        <Footer />
+        </>
+    )
+}
