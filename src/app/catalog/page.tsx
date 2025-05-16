4b2fb38dab843df6bc0426aff2a2ffac1a5b22fd
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-16 18:19:56 +0700

    created catalog page

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "../_components/Footer/Footer";
+import Nav from "../_components/Nav/Nav";
+
+export default function Catalog(){
+    return (
+        <>
+        <Nav />
+        <main>
+            Catalog
+        </main>
+        <Footer />
+        </>
+    )
+}
