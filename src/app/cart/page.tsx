993da3deaae10fd98ed470a32d072581f663e684
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-16 18:19:52 +0700

    created cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "../_components/Footer/Footer";
+import Nav from "../_components/Nav/Nav";
+
+export default function Cart(){
+    return (
+        <>
+            <Nav />
+            <main>
+                Cart
+            </main>
+            <Footer />
+        </>
+    )
+}
