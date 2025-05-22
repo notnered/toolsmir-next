86bdc6ba7062b68292d3e4fadeaf27e3ef0b8dbf
Konstantin Nered <konstantin.n616@gmail.com>
2025-05-22 10:57:04 +0700

    created todo about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
+import AboutBlock from "../_components/AboutBlock/AboutBlock";
 import Footer from "../_components/Footer/Footer";
 import Nav from "../_components/Nav/Nav";
 
 export default function About(){
     return (
         <>
-        <Nav />
-        <main>
-            About
-        </main>
-        <Footer />
+            <Nav />
+            <main>
+                <AboutBlock />
+            </main>
+            <Footer />
         </>
     )
 }
